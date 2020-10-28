f1d404d09370a13bd1c9e55b70a29e03133902f1
Rong Yu <rong.yu@happyelements.com>
2020-10-28 15:54:15 +0800

    初始化

--- src/const/index.js
+++ src/const/index.js
@@ -1,6 +1,14 @@
-// const MAIN_HOST = (process.env.NODE_ENV === 'development' || process.env.IS_DEV) ? 'mfwdev.com' : 'mafengwo.cn'
-const MAIN_HOST = 'localhost:8081'
+// const MAIN_HOST = (process.env.NODE_ENV === 'development' || process.env.IS_DEV) ? 'test.myproject.com' : 'www.myproject.cn'
+console.log(process.env.NODE_ENV)
+var host = '127.0.0.1:8199'
+if(process.env.NODE_ENV === 'development'){
+  host = 'dev.myproject.com'
+}else if(process.env.NODE_ENV === 'test'){
+  host = 'test.myproject.com'
+}else{
+  host = 'www.myproject.com'
+}
 const UTIL = {
-  AJAX_BASEURL: 'http://' + MAIN_HOST
+  AJAX_BASEURL: window.location.protocol+'//' + host
 }
 export default UTIL
